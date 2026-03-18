6097d415f0a418b8ba8299e94ccd4b1351be8ab6
João Pedro Belo <belo.joaopedro@gmail.com>
2026-03-18 00:03:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3740222807-6579a",
+  "appId": "1:963428244871:web:c8018c1ea472b7a4384722",
+  "apiKey": "AIzaSyC-hnU-6uO7KAEcX1rj38k8K7fOP0-jqZM",
+  "authDomain": "studio-3740222807-6579a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "963428244871"
+};
